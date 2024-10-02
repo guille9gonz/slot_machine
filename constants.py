547f39f0501ecdaa221f64b7713501232372e442
Guillermo <guillermogcordero@gmail.com>
2024-10-02 16:13:09 +0200

    Added comments

---
+++ constants.py
@@ -0,0 +1,20 @@
+MAX_LINES = 3
+MAX_BET = 200
+MIN_BET = 10
+
+ROWS = 3
+COLS = 3
+
+SYMBOL_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+SYMBOL_VALUES = {
+    "A": 10,
+    "B": 8,
+    "C": 4,
+    "D": 2
+}
